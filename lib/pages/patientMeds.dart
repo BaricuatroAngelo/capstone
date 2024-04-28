30a9b71140f6f3d897a9f4354f76a24bc50b0cb5
BaricuatroAngelo <113020235+BaricuatroAngelo@users.noreply.github.com>
2024-04-28 10:56:44 +0800

    Update patientMeds.dart
    will display a pop up dialog for the full patient med info

--- lib/pages/patientMeds.dart
+++ lib/pages/patientMeds.dart
@@ -23,9 +23,39 @@ class PatientMedicineListPage extends StatefulWidget {
 }
 
 class _PatientMedicineListPageState extends State<PatientMedicineListPage> {
-  @override
-  void dispose() {
-    super.dispose();
+  void _showMedicineDetails(Medicine medicine, PatientMedicine patientMedicine) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(medicine.medicineName ?? 'Unknown Medicine'),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Brand: ${medicine.medicineBrand ?? "Unknown Brand"}'),
+              Text('Dosage: ${medicine.medicineDosage ?? "Unknown Dosage"}'),
+              Text('Price: ${medicine.medicinePrice ?? "Unknown Price"}'),
+              Text('Type: ${medicine.medicineType ?? "Unknown Type"}'),
+              Text(
+                'Frequency: ${patientMedicine.medicineFrequency}',
+              ),
+              Text(
+                'End Date: ${patientMedicine.patientMedicineDate != null ? DateFormat("yyyy-MM-dd HH:mm:ss").format(DateTime.parse(patientMedicine.patientMedicineDate!)) : "Unknown"}',
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('Close'),
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -45,89 +75,83 @@ class _PatientMedicineListPageState extends State<PatientMedicineListPage> {
       ),
       body: widget.patientMedicines.isEmpty
           ? const Center(
-        child: Text(
-          'Patient has yet to take medications',
-          style: TextStyle(
-            fontSize: 30,
-            color: Colors.grey,
-          ),
-        ),
-      )
-          : ListView.builder(
-        itemCount: widget.patientMedicines.length,
-        itemBuilder: (context, index) {
-          final patientMedicine = widget.patientMedicines[index];
-          Medicine? medicine = widget.medicinesList.firstWhere(
-                (medicine) => medicine.medicineId == patientMedicine.medicineId,
-            orElse: () => Medicine(
-              medicineId: '',
-              medicineName: 'Unknown Medicine',
-              medicineBrand: 'Unknown Brand',
-              medicineDosage: 'Unknown Dosage',
-              medicinePrice: 'Unknown Price',
-              medicineType: 'Unknown Type',
-            ),
-          );
-
-          return Card(
-            elevation: 2,
-            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: ListTile(
-              tileColor: Colors.white,
-              leading: const Icon(
-                Icons.medical_services,
-                size: 40,
-                color: Colors.blue,
-              ),
-              title: Text(
-                medicine.medicineName ?? 'Unknown Medicine',
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+              child: Text(
+                'Patient has yet to take medications',
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.grey,
                 ),
               ),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Frequency: ${patientMedicine.medicineFrequency}',
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
+            )
+          : ListView.builder(
+              itemCount: widget.patientMedicines.length,
+              itemBuilder: (context, index) {
+                final patientMedicine = widget.patientMedicines[index];
+                final medicine = widget.medicinesList.firstWhere(
+                  (m) => m.medicineId == patientMedicine.medicineId,
+                  orElse: () => Medicine(
+                    medicineId: '',
+                    medicineName: 'Unknown Medicine',
+                    medicineBrand: 'Unknown Brand',
+                    medicineDosage: 'Unknown Dosage',
+                    medicinePrice: 'Unknown Price',
+                    medicineType: 'Unknown Type',
+                  ),
+                );
+
+                return Card(
+                  elevation: 2,
+                  margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                  child: ListTile(
+                    tileColor: Colors.white,
+                    leading: const Icon(
+                      Icons.medical_services,
+                      size: 40,
+                      color: Colors.blue,
+                    ),
+                    title: Text(
+                      medicine.medicineName ?? 'Unknown Medicine',
+                      style: const TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Frequency: ${patientMedicine.medicineFrequency}',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        Text(
+                          'Type: ${medicine.medicineType ?? "Unknown Type"}',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                        Text(
+                          'End Date: ${patientMedicine.patientMedicineDate != null ? DateTime.now().isAfter(DateTime.parse(patientMedicine.patientMedicineDate!)) ? "MEDICATION ENDED" : DateFormat("yyyy-MM-dd HH:mm:ss").format(DateTime.parse(patientMedicine.patientMedicineDate!)) : "Unknown Type"}',
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.edit),
+                      onPressed: () {
+                        _showMedicineDetails(medicine, patientMedicine);
+                      },
                     ),
                   ),
-                  Text(
-                    'Type: ${medicine.medicineType ?? 'Unknown Type'}',
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                  Text(
-                    'End Date: ${
-                        patientMedicine.patientMedicineDate != null
-                            ? DateTime.now().isAfter(DateTime.parse(patientMedicine.patientMedicineDate!))
-                            ? 'MEDICATION ENDED'
-                            : DateFormat('yyyy-MM-dd HH:mm:ss').format(DateTime.parse(patientMedicine.patientMedicineDate!))
-                            : 'Unknown Type'
-                    }',
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ),
-                ],
-              ),
-              trailing: IconButton(
-                icon: const Icon(Icons.edit),
-                onPressed: () {
-                  // Add functionality to show more medicine details
-                },
-              ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
