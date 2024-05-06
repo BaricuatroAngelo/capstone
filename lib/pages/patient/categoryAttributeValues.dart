4e9e6154694981a2b4f37513792e4a8a44f01015
BaricuatroAngelo <113020235+BaricuatroAngelo@users.noreply.github.com>
2024-05-06 20:37:44 +0800

    Update categoryAttributeValues.dart
    added function where if boolean == 0, will not display the category attribute

--- lib/pages/patient/categoryAttributeValues.dart
+++ lib/pages/patient/categoryAttributeValues.dart
@@ -116,81 +116,62 @@ class catAttValuesState extends State<catAttValues> {
   Widget buildListView() {
     if (!_dataFetched) {
       return const Center(child: CircularProgressIndicator());
-    } else if (_attributes.isEmpty) {
-      return const Center(child: Text('No data available'));
-    } else {
-      _attributes.sort((a, b) => a.categoryAtt_returnName.compareTo(b.categoryAtt_returnName));
+    }
 
-      return ListView.builder(
-        itemCount: _attributes.length,
-        itemBuilder: (context, index) {
-          String attributeName = _attributes[index].categoryAtt_returnName;
+    final List<Widget> listItems = [];
 
-          // Find the corresponding attribute value for the current category attribute
-          final attributeValue = _attributeValues.firstWhere(
-                (value) => value.categoryAtt_id == _attributes[index].categoryAtt_id,
-            orElse: () => AttributeValues(
-              attributeVal_values: 'N/A',
-              attributeVal_id: '',
-              patientId: '',
-              categoryAtt_id: '',
-            ), // Set default value if attribute value not found
-          );
+    for (var attribute in _attributes) {
+      String attributeName = attribute.categoryAtt_returnName;
 
-          var displayValue = attributeValue.attributeVal_values;
-          if (_attributes[index].categoryAtt_dataType == 'boolean') {
-            displayValue = displayValue == '1' ? 'Present' : 'Absent';
-          }
+      final attributeValue = _attributeValues.firstWhere(
+        (value) => value.categoryAtt_id == attribute.categoryAtt_id,
+        orElse: () => AttributeValues(
+          attributeVal_values: 'N/A',
+          attributeVal_id: '',
+          patientId: '',
+          categoryAtt_id: '',
+        ),
+      );
 
-          // Check if attribute name contains 'specify'
-          final bool isSpecify = attributeName.toLowerCase().contains('specify');
+      if (attribute.categoryAtt_dataType == 'boolean' &&
+          attributeValue.attributeVal_values == '0') {
+        continue; // Skip this attribute if the value is 0 and it's boolean
+      }
 
-          return Card(
-            elevation: 2,
-            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
-            child: ListTile(
-              title: GestureDetector(
-                onTap: () {
-                  if (isSpecify) {
-                    showDialog(
-                      context: context,
-                      builder: (context) {
-                        return AlertDialog(
-                          title: Text(attributeName),
-                          content: Text(attributeValue.attributeVal_values),
-                          actions: [
-                            TextButton(
-                              onPressed: () => Navigator.pop(context),
-                              child: Text('Close'),
-                            ),
-                          ],
-                        );
-                      },
-                    );
-                  }
-                },
-                child: Text(
-                  attributeName,
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                  ),
-                  overflow: TextOverflow.visible,
-                ),
+      var displayValue = attributeValue.attributeVal_values;
+      if (attribute.categoryAtt_dataType == 'boolean') {
+        displayValue = displayValue == '1' ? 'Present' : 'Absent';
+      }
+
+      listItems.add(
+        Card(
+          elevation: 2,
+          margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+          child: ListTile(
+            title: Text(
+              attributeName,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24,
               ),
-              trailing: Text(
-                displayValue,
-                style: const TextStyle(
-                  color: Colors.grey,
-                  fontSize: 24,
-                ),
-                overflow: TextOverflow.ellipsis,
-              ),
+              overflow: TextOverflow.visible,
             ),
-          );
-        },
+            trailing: Text(
+              displayValue,
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 24,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
       );
     }
+
+    return ListView(
+      children: listItems,
+    );
   }
 
 
